337d2760b81418ca3b8ed4f8de7940fbbfe6e718
elijahducote <validpupflower@gmail.com>
2017-10-20 17:44:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const client = new Discord.Client();
 client.on('ready', () => {
 
   console.log('The bot is up and running, yaayy!');
-  message.reply('Ring.'); // Bot join message
 
+});
+
+client.on('message', () => {
+  
+   message.reply('Ring.'); // Bot join message
+  
 });
 
 client.login(process.env.BOT_TOKEN);
